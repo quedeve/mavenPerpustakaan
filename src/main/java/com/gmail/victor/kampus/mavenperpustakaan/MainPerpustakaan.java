6d5f63c49e1408c504e12cdf1a9a79780df6988f
Victor Boy Simamora <kampus.victor@gmail.com>
2018-04-19 11:52:14 +0700

    Penambahan fitur yakin/tidak remove pada map

--- src/main/java/com/gmail/victor/kampus/mavenperpustakaan/MainPerpustakaan.java
+++ src/main/java/com/gmail/victor/kampus/mavenperpustakaan/MainPerpustakaan.java
@@ -15,7 +15,7 @@ public class MainPerpustakaan {
         boolean isTambah = true;
         Scanner input = new Scanner(System.in);
         Perpustakaan perpus = new Perpustakaan();
-        String isbn ;
+        String isbn;
         char keyakinan;
         Map<String, Book> daftarBuku = new LinkedHashMap<String, Book>();
         while (isTambah) {
@@ -27,16 +27,15 @@ public class MainPerpustakaan {
             System.out.print(" Pengarang : ");
             book.setAuthor(input.next());
             System.out.print(" No ISBN : ");
-            isbn =  input.next();
-            System.out.println("Yakin menambahkan data berikut ? "+ book.getAuthor() +" / "+ book.getTitle()+ " Y/F");
+            isbn = input.next();
+            System.out.println("Yakin menambahkan data berikut ? " + book.getAuthor() + " / " + book.getTitle() + " Y/F");
             keyakinan = input.next().charAt(0);
             if (keyakinan == 'Y') {
                 daftarBuku.put(isbn, book);
+            } else if (keyakinan == 'F') {
+                System.out.println(book.getAuthor() + " / " + book.getTitle() + " tidak jadi ditambahkan ");
             }
-            else if(keyakinan == 'F'){
-                System.out.println(book.getAuthor() +" / "+ book.getTitle()+ " tidak jadi ditambahkan ");
-            }
-            
+
             System.out.print(" Tambah data baru kembali ?(jawab dengan true/false : ");
             isTambah = input.nextBoolean();
 
@@ -49,7 +48,15 @@ public class MainPerpustakaan {
         System.out.println("____________________________________________________________________ ");
         System.out.println(" ***********************************************************");
         System.out.println(" Hapus buku dengan No ISBN :  ");
-        daftarBuku.remove(input.next());
+        isbn = input.next();
+        System.out.println("Yakin menghapus buku dengan No ISBN ? " + isbn +  " Y/F");
+        keyakinan = input.next().charAt(0);
+        if (keyakinan == 'Y') {
+            daftarBuku.remove(input.next());
+        } else if (keyakinan == 'F') {
+            System.out.println( "Buku dengan No "+ isbn + " tidak jadi dihapus ");
+        }
+
         System.out.println("____________________________________________________________________ ");
         for (Map.Entry<String, Book> entry : daftarBuku.entrySet()) {
             System.out.println(" ***********************************************************");
